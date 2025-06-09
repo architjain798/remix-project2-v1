bdc4c416efb4468d39071fd2ac13bed31b185d0d
Archit Jain <archit.jain@myadvice.com>
2025-06-09 23:46:44 +0530

    Add ExpensesHeader component and integrate it into ExpensesAppLayout; update MarketingLayout to include MainHeader

--- app/root.jsx
+++ app/root.jsx
@@ -34,7 +34,7 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <MainHeader />
+        {/* <MainHeader /> */}
         <Outlet />
         <ScrollRestoration />
         <Scripts />
