59ddf09ae17ceec451a3452d0f1c3f4bb08f1f26
Archit Jain <archit.jain@myadvice.com>
2025-06-09 22:26:55 +0530

    Add Expenses pages and layout components; implement Expenses Analysis with Chart and ExpenseStatistics

---
+++ app/routes/__app.jsx
@@ -0,0 +1,16 @@
+import expensesStyles from "~/styles/expenses.css";
+
+import { Outlet } from "@remix-run/react";
+
+export default function ExpensesAppLayout() {
+  return <Outlet />;
+}
+
+export function links() {
+  return [
+    {
+      rel: "stylesheet",
+      href: expensesStyles,
+    },
+  ];
+}
