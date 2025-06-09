07d1240b8245e24a8abebe603980a3a4a24d32fe
Archit Jain <architjain798@gmail.com>
2025-06-09 23:53:33 +0530

    Refactor ExpensesRawPage to use loader function for fetching expense data

--- app/routes/expenses.raw.jsx
+++ app/routes/expenses.raw.jsx
@@ -1,5 +1,26 @@
 // /expenses/raw
 
-export default function ExpensesRawPage() {
-  return <h1>Expenses Raw Page</h1>;
+export function loader() {
+  return new Response(
+    JSON.stringify([
+      {
+        id: "e1",
+        title: "Car Insurance",
+        amount: 294.67,
+        date: "2023-01-01",
+      },
+      {
+        id: "e2",
+        title: "New TV",
+        amount: 799.49,
+        date: "2023-01-05",
+      },
+      {
+        id: "e3",
+        title: "Groceries",
+        amount: 94.12,
+        date: "2023-01-10",
+      },
+    ])
+  );
 }
